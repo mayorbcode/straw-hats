6e04c82097fdab662fe647d22ee0bb533dce4621
Patrick Bello <mayorbello@yahoo.com>
2020-12-17 12:54:40 -0700

    sorted array by id using ternary

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -1,26 +1,28 @@
 import React from "react";
 
 const Gallery = ({info}) => {
+  // Sort items by their id then map over array to return array of figures and store value in a variable
+  const galleryArray = info.sort((a, b) => {
+    return (a.id < b.id) ? -1 : (a.id > b.id) ? 1 : 0;
+  })
+  .map((item) => {
+      return (
+        <a key={item.id} className="link" href={item.wiki} target='_blank' rel='noreferrer'>
+          <figure className='card'>
+            <img className='image' src={`https://anime-characters-api.herokuapp.com/images/${item.nameQuery}.jpg`} alt={item.nameQuery}/>
+            <figcaption>
+              <h2>{item.name}</h2>
+              <p>{item.role}</p>
+            </figcaption>
+          </figure>
+        </a>
+      );
+    });
   return (
-    <div>
-      <div className="gallery">
-        {/* Map over array and output to DOM */}
-        {info.map((item) => {
-          return (
-            <a key={item.id} className="link" href={item.wiki} target='_blank' rel='noreferrer'>
-              <figure className='card'>
-                <img className='image' src={`https://anime-characters-api.herokuapp.com/images/${item.nameQuery}.jpg`} alt={item.nameQuery}/>
-                <figcaption>
-                  <h2>{item.name}</h2>
-                  <p>{item.role}</p>
-                </figcaption>
-              </figure>
-            </a>
-          )
-        })}  
-      </div>
-   </div>
-  )
+    <div className="gallery">
+      {galleryArray}  
+    </div>
+  );
 }
 
 export default Gallery;
