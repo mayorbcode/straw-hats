e4bd01770d572cb771fefc96ca592b812a46a49b
Patrick Bello <mayorbello@yahoo.com>
2020-12-16 18:26:31 -0700

    committing source code

---
+++ src/components/app/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import Gallery from '../gallery/Gallery';
+
+function App() {
+  let [info, setInfo] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetch Data
+    fetch('https://anime-characters-api.herokuapp.com/api/v0/onepiece')
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      setInfo(data) 
+    })
+    // Switch isLoading to false once fetch is done
+    setIsLoading(false);
+  }, []);
+
+  return (
+    <>
+      <h1>Straw Hat Pirates</h1>
+      <Gallery props={info} loading={isLoading}/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/gallery/Gallery.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Gallery = ({props, loading}) => {
+  return (
+    <div>
+    {loading ? <h1>Loading...</h1> :
+      <div className="gallery">
+        {/* Map over array and output to DOM */}
+        {props.map((item) => {
+          return (
+            <a key={item.id} className="link" href={item.wiki} target='_blank' rel='noreferrer'>
+              <figure className='card'>
+                <img className='image' src={`https://anime-characters-api.herokuapp.com/images/${item.nameQuery}.jpg`} alt={item.nameQuery}/>
+                <figcaption>
+                  <h2>{item.name}</h2>
+                  <p>{item.role}</p>
+                </figcaption>
+              </figure>
+            </a>
+          )
+        })}  
+      </div>
+      }
+   </div>
+  )
+}
+
+export default Gallery;
